9a603088f41149ba465a61d9f2519a888e3be155
Denis Reinoso <reinoso.denis@gmail.com>
2019-01-25 16:20:05 -0300

    Updates

---
+++ precache-manifest.8b341aa7fcb0b7460a04531c2c625d3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f8e431aeb2f7a68d17b5",
+    "url": "/static/js/main.f8e431ae.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "f8e431aeb2f7a68d17b5",
+    "url": "/static/css/main.34a01ac9.chunk.css"
+  },
+  {
+    "revision": "f3c99924805320b61129a81087537aa2",
+    "url": "/index.html"
+  }
+];
