ca7f231e2d3df3a8926cecd4ca8b40bf95500660
Denis Reinoso <reinoso.denis@gmail.com>
2019-02-01 11:39:17 -0300

    Remove website
    Signed-off-by: Denis Reinoso <reinoso.denis@gmail.com>

--- src/resumeData.js
+++ src/resumeData.js
@@ -26,7 +26,7 @@ let resumeData = {
     ],
   "aboutme":"I have finished my studies as Computer Engineer and have been working in software industry for past few years. I alwasys try to be up to date with last technologies.",
   "address":"Córdoba, Argentina",
-  "website":"https://www.yourchainreaction.com",
+  "website":"",
   "education":[
     {
       "UniversityName":"Oracle Certified Professional",
